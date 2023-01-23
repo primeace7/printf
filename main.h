00ee386b93f7a40b0da558a81fb5877af4295027
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-23 09:08:04 +0000

    Add task 0 code file and update main.h file

--- main.h
+++ main.h
@@ -4,5 +4,9 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stddef.h>
+#include <stdarg.h>
 #include <stdlib.h>
 int _printf(const char *format, ...);
+int string_len(char *);
+int format_string(char *, va_list arg);
+#endif
